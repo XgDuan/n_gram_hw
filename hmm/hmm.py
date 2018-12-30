c7b83b2ebc99cdf5ccdceb08e9864fca5431de9a
XgDuan <lazydxg@gmail.com>
2018-12-30 22:43:31 +0800

    Add mediate

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -10,10 +10,12 @@ class DataLoader(object):
 
 
 class HMM(object):
-    def __init__(self, hidden_size):
-        self._pi = np.array(hidden_size)
-        self._a = np.array([hidden_size, hidden_size])
-        self._b = np.array([hidden_size,])
+    def __init__(self, hidden_size, observation_size, batch_size):
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+        self.pi = np.random.rand(hidden_size)  # random initialization
+        self.A = np.random.rand(hidden_size, hidden_size)
+        self.B = np.random.rand(hidden_size, observation_size)
 
     def hidden_state_inference(self, observation_seq):
         pass
@@ -23,15 +25,27 @@ class HMM(object):
         for corpus_unit in corpus_iterator:
             alpha, beta = self._compute_mediate(corpus_unit)
 
-    def _compute_mediate(self, corpus_mat):
-        
-        return alpha, beta
-    
-    def _expectation(self, ):
-        pass
+    def _compute_mediate(self, x_seq):
+        """
+        input:
+            x_seq: np.array of size (N_x, )
+        return:
+            alpha: np.array of size: (N_x, hidden_size)
+            beta: np.array of size: (N_x, hidden_size)
+        """
+        # forward pass
+        alpha = np.zeros((len(x_seq), self.hidden_size), dtype=np.float)
+        alpha[0, :] = self.pi * self.B[:, x_seq[0]]  # (hidden_size, )
+        for i in range(1, len(x_seq)):
+            # (hidden_size, )
+            alpha[i, :] = self.B[:, x_seq[i]] * np.matmul(alpha[i-1, np.newaxis, :], self.A)
 
-    def _maximum(self):
-        pass
+        # backward pass
+        beta = np.zeros_like(alpha)
+        beta[-1, :] = 1
+        for i in range(len(x_seq)-1, -1, -1):
+            beta[i, :] = np.matmul(self.A, beta[i + 1, :, np.newaxis] * self.B[:, x_seq[i + 1]])
+        return alpha, beta
 
     def sequence_probability(self):
         pass
